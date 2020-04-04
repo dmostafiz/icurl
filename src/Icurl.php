134480bf2d20152be360248450c5b7dda33ecfe0
mostafizrr2 <mostafizrrahman2@gmail.com>
2020-04-04 20:44:10 +0600

    Update core

--- src/Icurl.php
+++ src/Icurl.php
@@ -7,6 +7,11 @@ class Icurl{
     function __construct($url = null)
     {
         $this->ch = curl_init();
+        
+        if ($this->ch === false) {
+          die("Curl handler initialize failed");
+        }
+
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -32,12 +37,14 @@ class Icurl{
             $port = $proxy[1];
             curl_setopt($this->ch, CURLOPT_PROXY, $ip); 
             curl_setopt($this->ch, CURLOPT_PROXYPORT, $port);
-            curl_setopt($this->ch, CURLOPT_HTTPPROXYTUNNEL , 1);
+            // curl_setopt($this->ch, CURLOPT_HTTPPROXYTUNNEL , 1);
         } 
         else 
         {
-            die("Please give a array of valid proxies like [ '120.10.1.123:12345' ]");
+            die("Please give a array of valid proxies like [ '120.10.1.123:12345', '000.12.20.122:80' ]");
         }
+        
+        // curl_setopt($this->ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Linux; U; Android 2.1-update1; ru-ru; GT-I9000 Build/ECLAIR) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17");
 
         return $this;
     }
@@ -50,7 +57,15 @@ class Icurl{
 
     function get()
     {
-        return curl_exec($this->ch);
+        $exc = curl_exec($this->ch);
+        
+        if ($exc === false) {
+            
+          die(curl_error($this->ch));
+        }
+        
+        
+        return $exc;
     }
 
     function __destruct()
